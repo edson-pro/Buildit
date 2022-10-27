e4b9693f72308f50e0f8c23446992a75f03b0a61
ntwali edson <ntwaliedson9@gmail.com>
2022-10-27 11:53:06 +0200

    sx

--- Components/TeamItem.tsx
+++ Components/TeamItem.tsx
@@ -4,7 +4,7 @@ function TeamItem({ photo, name, role }: any) {
   return (
     <div className="shadow-xl shadow-[#9bc2ff40] bg-gradient-to-b from-[#C6DBFF] to-[#E1EBFA]">
       <div className="h-52 relative w-full overflow-hidden">
-        <Image objectFit="cover" layout="fill" src={photo} alt="" />
+        <img className="w-full h-full" src={photo} alt="" />
       </div>
       <div className="py-3 px-2">
         <h4 className="text-primaryDark font-semibold text-sm">{name}</h4>
